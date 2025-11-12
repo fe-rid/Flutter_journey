ab5a2da3307192417baed08eaefcdd3e2018879c
Alferid Zeinu <feridzeinu@gmail.com>
2025-11-12 22:24:28 +0300

    Updated project files and added new widgets and screens

---
+++ lib/screens/home/all_tickets.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/base/utilis/all_json.dart';
+import 'package:flutter_app/base/widgets/ticket_view.dart';
+
+class AllTickets extends StatelessWidget {
+  const AllTickets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("All Tickets")),
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: ticketList
+                  .map(
+                    (singleTicket) =>
+                        Container(
+                          margin: EdgeInsets.only(bottom: 20),
+                            child: TicketView(ticket: singleTicket, wholeScreen: true,
+                            )),
+                  )
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,113 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/base/res/media.dart';
+import 'package:flutter_app/base/res/styles/app_styles.dart';
+import 'package:flutter_app/base/utilis/all_json.dart';
+import 'package:flutter_app/base/widgets/app_double_text.dart';
+import 'package:flutter_app/base/widgets/ticket_view.dart';
+import 'package:flutter_app/screens/widgets/hotel.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyles.bgColor,
+      body: ListView(
+        children: [
+          const SizedBox(height: 40),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good morning", style: AppStyles.headLineStyle3),
+                        SizedBox(height: 5),
+                        Text("Book Tickets", style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(AppMedia.logo),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
+                      Text("Search"),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                AppDoubleText(
+                  bigText: 'Upcoming Flights',
+                  smallText: 'View all ',
+                  func: () => Navigator.pushNamed(context, "/all_tickets"),
+                ),
+                const SizedBox(height: 20),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: ticketList
+                         .take(2)
+                        .map((singleTicket) =>
+                        TicketView(ticket: singleTicket))
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                AppDoubleText(
+                  bigText: ' Hotels',
+                  smallText: 'View all ',
+                  func: () {
+
+                  },
+                ),
+                const SizedBox(height: 20),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    
+                    children: hotelList
+                        .take(2)
+                        .map((singleHotel) {
+                      return Hotel(hotel: singleHotel);
+                    }).toList(),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
